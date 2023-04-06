98b4fd8e52420a4ae86e4c49643e7a127ed861a3
Marijn Schipper <marijnschipper@gmail.com>
2023-04-06 14:34:24 +0200

    Added annotation main script to repo

---
+++ Annotate_ML_loci_filt.py
@@ -0,0 +1,17 @@
+import annotate_input_finemapped_fuma_filt as annotate
+import os
+import sys
+
+filedir = sys.argv[1]
+#check if fuma run correctly
+
+if not os.path.exists(os.path.join(filedir, 'genes.txt')):
+    print(f'FUMA has not properly run on this folder: {filedir}')
+    exit
+
+finemap_output = os.path.join(filedir, 'finemap_output')
+finemap_loci = os.listdir(finemap_output)
+for f in finemap_loci:
+    if 'log_sss' in f:
+        log_path = os.path.join(finemap_output, f)
+        annotate.main(log_path)
