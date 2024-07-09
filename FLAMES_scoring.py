dd6f4d8cc1de2d9ac058422a6f8e927d84545325
Marijn Schipper <schipper@int4.local.snellius.surf.nl>
2024-07-09 14:20:40 +0200

    make up to date

--- FLAMES_scoring.py
+++ FLAMES_scoring.py
@@ -2,9 +2,73 @@ import pandas as pd
 from sys import argv
 import pickle as pk
 import os
-import train_FLAMES as tf
+import annotate as ann
 import optimize_FLAMES as optf
 
+def add_into_single_df(list_of_dfs):
+    df = pd.concat(list_of_dfs)
+    df = df.reset_index(drop=True)
+    df["locus"] = pd.factorize(df["filename"])[0]
+    return df
+
+def create_XY_train(df, filter=None, inclusion="all"):
+    train_features = [
+        "genes_in_locus",
+        "credset_size",
+        "genes_within_50kb",
+        "genes_within_100kb",
+        "genes_within_250kb",
+        "Highest_PIP",
+        "rel_MAGMA_Z",
+        "MAGMA_Z",
+    ]
+    best_predictors = [
+        "weighted_distance",
+        "TSS_distance",
+        "weighted_TSS_distance",
+        "VEP_max",
+        "CADD_sum",
+        "CLPP_GTEX_tissue_weighted_eQTL_sum",
+        "RoadmapEpi_sum",
+        "HACER_PRO-seq_GRO-seq_sum",
+        "Promoter_sum",
+        "F5_ep_sum",
+        "Jung_HiC_sum",
+        "GeneHancer_sum",
+        "EpiMap_sum",
+        "mean_CLPP_eQTL_eQTLCatalog_max",
+        "ABC_CRISPR_sum",
+        "ABC_EP_sum",
+        "max_CLPP_rQTL_eQTLCatalog_max",
+        "Javierre_HiC_sum",
+        "HACER_CAGE_max",
+        "CLPP_eQTLgen_sum",
+        "cicero_sum",
+    ]
+    if filter != None:
+        df = df[df["distance"] <= int(filter)]
+    else:
+        df = df[df["weighted_distance"] <= 750000]
+        df = df.groupby("filename").apply(adjust_PoPS_scores)
+    if "gene_biotype" in df.columns:
+        df = df[df["gene_biotype"] == "protein_coding"]
+    if "type" in df.columns:
+        df = df[df["type"] == "protein_coding"]
+    df = df.groupby("filename").apply(ann.create_relative_annotation)
+
+    for feature in best_predictors:
+        if inclusion.lower() == "all":
+            train_features.append(feature)
+            train_features.append("rel_" + feature)
+        if inclusion.lower() == "rel":
+            train_features.append("rel_" + feature)
+        if inclusion.lower() == "raw":
+            train_features.append(feature)
+    na_columns = df[train_features].columns[df[train_features].isna().any()].tolist()
+    df[na_columns] = df[na_columns].fillna(0)
+    train_X = df[train_features]
+    train_y = list(df["TP"])
+    return train_X, train_y, df
 
 def main(model_dir, input, pops_combi, filt, outdir, name="FLAMES_scores"):
     for file in os.listdir(model_dir):
@@ -24,11 +88,11 @@ def main(model_dir, input, pops_combi, filt, outdir, name="FLAMES_scores"):
     dfs = [pd.read_csv(f, sep="\t") for f in infiles]
     for i in range(len(dfs)):
         dfs[i]["filename"] = infiles[i]
-    df = tf.add_into_single_df(dfs)
+    df = add_into_single_df(dfs)
     # Will be removed later if not used for benchmarking
     if not "TP" in list(df.columns):
         df["TP"] = 1
-    X_val, y_val, df = tf.create_XY_train(df, filt)
+    X_val, y_val, df = create_XY_train(df, filt)
     try:
         with open(model_dir + "/features.txt") as f:
             features = [l.strip() for l in f]
@@ -61,12 +125,21 @@ def main(model_dir, input, pops_combi, filt, outdir, name="FLAMES_scores"):
             "PoPS_Score",
             "rel_XGB_score",
             "rel_PoPS_Score",
-            "raw_FLAMES_score",
-            "FLAMES_score",
+            "FLAMES_raw",
+            "FLAMES_scaled",
+            "estimated_cumulative_precision",
+            "FLAMES_highest",
+            "FLAMES_causal",
+
         ]
     ]
-    print(f"output written to {os.path.join(outdir, name)}.txt")
-    df.to_csv(f"{outdir}/{name}.txt", sep="\t", index=False)
+    df['estimated_cumulative_precision'] = df['estimated_cumulative_precision'] * df['FLAMES_causal']
+    df.to_csv(f"{outdir}/{name}.raw", sep="\t", index=False)
+    df=df[df['FLAMES_causal'] == 1]
+    df = df.sort_values("FLAMES_scaled", ascending=False)
+    df = df[['locus', 'filename', 'symbol', 'ensg', 'FLAMES_scaled', 'FLAMES_raw', 'estimated_cumulative_precision']]
+    df.to_csv(f"{outdir}/{name}.pred", sep="\t", index=False)
+    print(f"output written to {os.path.join(outdir, name)}.pred and {os.path.join(outdir, name)}.raw")
     return
 
 
--- optimize_FLAMES.py
+++ optimize_FLAMES.py
@@ -3,11 +3,60 @@ import numpy as np
 from sys import argv
 import pickle as pk
 import os
-import train_FLAMES as tf
 import random
-from sklearn.utils.parallel import delayed
 import xgboost as xgb
+from sklearn.preprocessing import MinMaxScaler
 
+def add_into_single_df(list_of_dfs):
+    df = pd.concat(list_of_dfs)
+    df = df.reset_index(drop=True)
+    df["locus"] = pd.factorize(df["filename"])[0]
+    return df
+
+def create_XY_train(df, filter=None, inclusion="all"):
+    train_features = [
+        "genes_in_locus",
+        "credset_size",
+        "genes_within_50kb",
+        "genes_within_100kb",
+        "genes_within_250kb",
+        "Highest_PIP",
+        "rel_MAGMA_Z",
+        "MAGMA_Z",
+    ]
+    best_predictors = [
+        "weighted_distance",
+        "TSS_distance",
+        "weighted_TSS_distance",
+        "VEP_max",
+        "CADD_sum",
+        "CLPP_GTEX_tissue_weighted_eQTL_sum",
+        "RoadmapEpi_sum",
+        "HACER_PRO-seq_GRO-seq_sum",
+        "Promoter_sum",
+        "F5_ep_sum",
+        "Jung_HiC_sum",
+        "GeneHancer_sum",
+        "EpiMap_sum",
+        "mean_CLPP_eQTL_eQTLCatalog_max",
+        "ABC_CRISPR_sum",
+        "ABC_EP_sum",
+        "max_CLPP_rQTL_eQTLCatalog_max",
+        "Javierre_HiC_sum",
+        "HACER_CAGE_max",
+        "CLPP_eQTLgen_sum",
+        "cicero_sum",
+    ]
+    if filter != None:
+        df = df[df["distance"] <= int(filter)]
+    else:
+        df = df[df["weighted_distance"] <= 750000]
+        df = df.groupby("filename").apply(adjust_PoPS_scores)
+    if "gene_biotype" in df.columns:
+        df = df[df["gene_biotype"] == "protein_coding"]
+    if "type" in df.columns:
+        df = df[df["type"] == "protein_coding"]
+    df = df.groupby("filename").apply(ann.create_relative_annotation)
 
 def Flames_scoring(df, pops_combi):
     df = df.drop_duplicates(keep="first")
@@ -22,21 +71,27 @@ def Flames_scoring(df, pops_combi):
         lambda x: 1 if float(x) == float(1) else 0
     )
     df["dist_pred"] = df["rel_distance"].apply(lambda x: 1 if x == 1 else 0)
-    df["Combined_score"] = (1 - float(pops_combi)) * (df["rel_PoPS_Score"]) + float(
-        pops_combi
-    ) * df["rel_XGB_score"]
-    max_in_loc = float(max(df["Combined_score"]))
-    df["raw_FLAMES_score"] = df["Combined_score"]
-    if len(df) == 1:
-        df["FLAMES_score"] = df["XGB_score"]
+    scaler = MinMaxScaler(feature_range=(0.292, 1))
+    scaled_values = scaler.fit_transform(df[['PoPS_Score']])
+    df['rel_PoPS_Score'] = scaled_values
+    df['rel_PoPS_Score'] = df['rel_PoPS_Score'].round(10)
+    df['rel_PoPS_Score'] = df['rel_PoPS_Score']/(max(df['rel_PoPS_Score']))
+    if len(df) ==1 :
+        df['FLAMES_scaled'] = 1
+        df['FLAMES_raw'] = 1.583 * df['XGB_score']
+    elif max(df['rel_PoPS_Score']) == 0:
+        df['FLAMES_raw'] = df['XGB_score']
+        df['FLAMES_scaled'] = ((df['FLAMES_raw']/sum(df['FLAMES_raw'])))
     else:
-        df["diff"] = df["Combined_score"] - df["Combined_score"].nlargest(2).values[-1]
-        df["diff2"] = df["Combined_score"].nlargest(1).values[0] - df["Combined_score"]
-        df["FLAMES_score"] = df["diff"] - df["diff2"]
-    max_in_loc = float(max(df["FLAMES_score"]))
-    df["FLAMES_causal"] = df["FLAMES_score"].apply(
-        lambda x: 1 if x == max_in_loc and max_in_loc > 0 else 0
-    )
+        df['FLAMES_raw'] =  df['XGB_score'] * df['rel_PoPS_Score'] +  0.583 * df['XGB_score'] * df['rel_PoPS_Score'].astype(int)
+        df['FLAMES_scaled'] = ((df['FLAMES_raw']/sum(df['FLAMES_raw']))) 
+    max_in_loc = max(df['FLAMES_scaled'])
+    df["FLAMES_highest"] = df["FLAMES_scaled"]/max_in_loc
+    df['FLAMES_highest'] = df['FLAMES_highest'].astype(int)
+    df['FLAMES_causal'] = ((df['FLAMES_raw'] > 0.134) & (df['FLAMES_scaled'] > 0.248)).astype(int) * df['FLAMES_highest']
+    #Based on calibration as performed in paper (simple polynomial fit)
+    df['estimated_cumulative_precision'] = 1.5776340061236926 * df['FLAMES_scaled']**3 -2.761585223042816 * df['FLAMES_scaled']**2 + 1.8136337684824269 * df['FLAMES_scaled'] + 0.44600372410005074 
+    df['estimated_cumulative_precision'] = df['estimated_cumulative_precision'].apply(lambda x: 1 if x > 1 else x)
     return df
 
 
@@ -114,8 +169,8 @@ def main(model, trainfiles, outdir, filter):
                 continue
             train.extend(data_list[j])
         val = data_split
-        train = tf.add_into_single_df(train)
-        train_X, train_y, df = tf.create_XY_train(train, filter)
+        train = add_into_single_df(train)
+        train_X, train_y, df = create_XY_train(train, filter)
         # Load features used in model
         if os.path.exists(outdir + "/features.txt"):
             with open(outdir + "/features.txt") as f:
@@ -123,8 +178,8 @@ def main(model, trainfiles, outdir, filter):
             train_X = train_X[features]
         # train model
         model = XGB_model.fit(train_X, train_y)
-        val = tf.add_into_single_df(val)
-        val_X, val_y, df = tf.create_XY_train(val, filter)
+        val = add_into_single_df(val)
+        val_X, val_y, df = create_XY_train(val, filter)
         if os.path.exists(outdir + "/features.txt"):
             with open(outdir + "/features.txt") as f:
                 features = [l.strip() for l in f]
